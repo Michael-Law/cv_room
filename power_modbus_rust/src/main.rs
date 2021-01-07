ec2356f95cfd26319a7797a9cf26e5bd39283c53
Michael-Law <mlawsan@outlook.com>
2021-01-07 13:30:04 +0400

    Looping through many devices

--- power_modbus_rust/src/main.rs
+++ power_modbus_rust/src/main.rs
@@ -11,15 +11,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     use tokio_modbus::prelude::*;
 
     let tty_path = "COM6";
-    let slave = Slave(0x17);
+    
+    let addresses = vec![1,2,3,4,5];
+    loop {
+        for i in 0..addresses.len(){
+            
+            let slave = Slave(addresses[i]);
 
-    let mut settings = SerialPortSettings::default();
-    settings.baud_rate = 19200;
-    let port = Serial::from_path(tty_path, &settings).unwrap();
+            let mut settings = SerialPortSettings::default();
+            settings.baud_rate = 19200;
+            let port = Serial::from_path(tty_path, &settings).unwrap();
 
-    let mut ctx = rtu::connect_slave(port, slave).await?;
-    println!("Reading a sensor value");
-    let rsp = ctx.read_holding_registers(0x082B, 2).await?;
-    println!("Sensor value is: {:?}", rsp);
+            let mut ctx = rtu::connect_slave(port, slave).await?;
+            println!("Reading a sensor value");
+            let rsp = ctx.read_holding_registers(0x082B, 2).await?;
+            println!("Sensor value is: {:?}", rsp);
+        }
+    }
     Ok(())
 }
